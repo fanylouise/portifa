e9e75bab2c93e8d9da431d67af408d729cb4bce0
fanylouise <gandra.fany@gmail.com>
2024-09-15 21:46:18 -0300

    assets and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import { Navbar } from "./sections/Navbar.jsx";
+
 const App = () =>{
   return (
-    <main className='max-w-7xl mx-auto'>
-      <h1 className='text-2xl text-white underline '>Hello World</h1>
+    <main className="max-w-7xl mx-auto">
+      <Navbar/>
     </main>
   )
 }
